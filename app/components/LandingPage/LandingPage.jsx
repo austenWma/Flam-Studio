3180bda8cd316445635c505a7325214527585882
Austen Ma <austenma@Austens-MacBook-Pro.local>
2017-12-15 21:04:25 -0800

    Successful (hardcoded) temp button on LandingPage that downloads dynamically to directory

--- app/components/LandingPage/LandingPage.jsx
+++ app/components/LandingPage/LandingPage.jsx
@@ -13,8 +13,9 @@ var zipFolder = window.require('zip-folder');
 
 import { firebaseRef } from '../../Firebase/firebase.js'
 import * as firebase from 'firebase'
-
 import path from 'path'
+
+var download = window.require('download-file')
 
 class LandingPage extends Component {
   constructor (props) {
@@ -23,6 +24,7 @@ class LandingPage extends Component {
     };
     this.goToProjectsPage = this.goToProjectsPage.bind(this)
     this.logOut = this.logOut.bind(this)
+    this.dlDemo = this.dlDemo.bind(this)
   }
 
   goToProjectsPage() {
@@ -34,6 +36,22 @@ class LandingPage extends Component {
       localStorage.setItem('access_token', null)
       this.props.history.push('/')
     })
+  }
+
+  dlDemo() {
+
+    let appPath = remote.app.getAppPath()
+
+    var url = "https://firebasestorage.googleapis.com/v0/b/flam-studio.appspot.com/o/0.184wyx2tdj1%2FTesting%202%2FTesting%2021.logicx.zip?alt=media&token=2fa5b121-9cf5-4f65-8943-25fd68b4f90a"
+    
+    var options = {
+        directory: appPath + '/Synced-Files/',
+        filename: "Testing 21.logicx.zip"
+    }
+      
+    download(url, options, function(err){
+        if (err) throw err
+    }) 
   }
 
   render() {
@@ -55,6 +73,7 @@ class LandingPage extends Component {
             Log Out
           </button>
         </div>
+        <button onClick={this.dlDemo}>Download Demo</button>
       </div>
     )
   }
